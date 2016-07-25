55808276f00ef2b5bfda156315d24ba1e845f3d5
danielburz <daniel.burz@student.unitbv.ro>
2016-07-25 09:16:03 +0300

    backend changes

--- practicap5/Pentagram/admin.py
+++ practicap5/Pentagram/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from Pentagram.models import Photo, Comments
+
+admin.site.register(Photo)
+admin.site.register(Comments)
+#admin.site.register(Likes)
--- practicap5/practicap5/settings.py
+++ practicap5/practicap5/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'Pentagram',
 ]
 
@@ -121,3 +123,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
+
+LOGIN_REDIRECT_URL = "homepage"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':
+        ('rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHETICATION_CLASSES': (
+        'rest_framework.authentication.TokenAithentication',
+    )
+}
